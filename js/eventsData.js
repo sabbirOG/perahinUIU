76f3996f79947d1ebc67030d2ac219a788ee04cc
sabbirOG <msabbirahmed53@gmail.com>
2025-10-28 22:00:41 +0600

    remove hardcodded data

---
+++ js/eventsData.js
@@ -0,0 +1,20 @@
+// js/eventsData.js
+// Array of event objects for UIU events
+const eventsData = [
+  {
+    title: "Tech Career Fair",
+    date: "Dec 15, 2025",
+    time: "10:00 AM"
+  },
+  {
+    title: "Project Showcase",
+    date: "Dec 20, 2025",
+    time: "2:00 PM"
+  },
+  {
+    title: "Trimester Break",
+    date: "Dec 25 - Jan 5",
+    time: ""
+  }
+  // Add more events as needed
+];
