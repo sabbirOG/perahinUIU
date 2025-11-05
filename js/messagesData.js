839a87f43aeeef146b3b7b934a79a3a3a26b6a54
sabbirOG <msabbirahmed53@gmail.com>
2025-11-06 02:43:05 +0600

    added message

--- js/messagesData.js
+++ js/messagesData.js
@@ -4,7 +4,7 @@ const messagesData = [
         sender: 'Dept. Office',
         content: 'Section selection and registration are tentatively scheduled for the first week of November, following the final exams.',
         timestamp: new Date('2025-10-24T10:30:00'),
-        isImportant: true
+        isImportant: false
     },
     {
         sender: 'UIU',
@@ -17,5 +17,11 @@ const messagesData = [
         content: 'Final exams start on 25 Oct 2025.',
         timestamp: new Date('2025-10-16T09:00:00'),
         isImportant: false
+    },
+    {
+        sender: 'Dept. CSE',
+        content: 'Registration & advising open 8 Nov, 9AM via Ucam Cloud.(SECTION Selection)',
+        timestamp: new Date('2025-11-05T09:00:00'),
+        isImportant: true
     }
 ];
